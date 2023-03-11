9e7155411c865444ae79c7225a9b091df7e529b4
Adson Gomes Olivera <adsonprofessionalemail@gmail.com>
2023-03-11 14:03:49 -0300

    <feat> Created structure for header and added a svg logo

---
+++ src/components/MainHeader.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function MainHeader () {
+  return (
+    <section className="main-header">
+      <div className="header-logo" />
+      <div className="header-search-bar" />
+      <div className="login" />
+      <div className="shopping-cart" />
+    </section>
+  );
+};
