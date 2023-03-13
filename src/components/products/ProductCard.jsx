124026d4ed17f78b76c2de5a49fdb35fe38c29c4
Adson Gomes Olivera <adsonprofessionalemail@gmail.com>
2023-03-13 12:04:59 -0300

    <feat> Products created

--- src/components/products/ProductCard.jsx
+++ src/components/products/ProductCard.jsx
@@ -1,21 +1,33 @@
 import React from 'react';
 import { v4 as uuid } from 'uuid';
 import PropTypes from 'prop-types';
+import '../styles/Products.css';
+
+const NO_IMG_URL = 'https://thumbs.dreamstime.com/b/no-thumbnail-image-placeholder-forums-blogs-websites-148010362.jpg';
 
 function ProductCard({ info }) {
   const {
-    name,
-    description,
-    slug,
-    unitPrice,
+    product,
+    thumbnail,
+    unit_price: unitPrice,
   } = info;
+
+  const formatNumberToPrice = (number) => {
+    const numberToDigit = number.toFixed(2);
+    const turningIntoString = numberToDigit.toString();
+    const numberFormatedToPrice = turningIntoString.replace('.', ',');
+    return numberFormatedToPrice;
+  };
 
   return (
     <div key={uuid()} className="product-card">
-      <span>{name}</span>
-      <span>{description}</span>
-      <span>{slug}</span>
-      <span>{unitPrice}</span>
+      <span className="title">{product}</span>
+      <img src={thumbnail || NO_IMG_URL} alt={product} />
+      <div className="price">
+        <span>À Vista ou no Cartão de Crédito</span>
+        <span>Até 12 X sem juros</span>
+        <span>{`R$ ${formatNumberToPrice(unitPrice)}`}</span>
+      </div>
     </div>
   );
 }
--- src/components/products/Products.jsx
+++ src/components/products/Products.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useContext, useState } from 'react';
 import PagoShopContext from '../../context/PagoShopContext';
 import ProductCard from './ProductCard';
+import '../styles/Products.css';
 
 function Products() {
   const [products, setProducts] = useState([]);
