a01b169c1bfc53159f5a6e0baf7c82c8653759a4
Adson Gomes Oliveira <adsongoliveira2022@outlook.com>
2023-04-01 14:14:31 -0300

    <setup> Added docker-compose to frontend

--- src/api/orders.js
+++ src/api/orders.js
@@ -13,8 +13,8 @@ const getOneOrder = async ({ id, token }) => {
 };
 
 const createOrder = async ({ orderInfos, token }) => {
-  const requestStringOne = `http://${process.env.REACT_APP_ORDERS_HOST || '127.0.0.1'}`;
-  const requestStringTwo = `:${process.env.REACT_APP_ORDERS_PORT}/${process.env.REACT_APP_ORDERS_BASEURL}`;
+  const requestStringOne = `http://${process.env.REACT_APP_GATEWAY_HOST || '127.0.0.1'}`;
+  const requestStringTwo = `:${process.env.REACT_APP_GATEWAY_PORT}/${process.env.REACT_APP_ORDERS_BASEURL}`;
   const requestString = requestStringOne + requestStringTwo;
 
   const response = await axios.post(requestString, orderInfos, {
@@ -25,9 +25,9 @@ const createOrder = async ({ orderInfos, token }) => {
 };
 
 const confirmOrder = async ({ paymentInfos, id, token }) => {
-  const requestStringOne = `http://${process.env.REACT_APP_ORDERS_HOST || '127.0.0.1'}`;
-  const requestStringTwo = `:${process.env.REACT_APP_ORDERS_PORT}/${process.env.REACT_APP_ORDERS_BASEURL}`;
-  const requestStringThree = `${process.env.REACT_APP_ORDERS_CONFIRM_RESOURCE}/${id}`;
+  const requestStringOne = `http://${process.env.REACT_APP_GATEWAY_HOST || '127.0.0.1'}`;
+  const requestStringTwo = `:${process.env.REACT_APP_GATEWAY_PORT}/${process.env.REACT_APP_ORDERS_BASEURL}`;
+  const requestStringThree = `/confirm/${id}`;
   const requestString = requestStringOne + requestStringTwo + requestStringThree;
 
   const response = await axios.post(requestString, paymentInfos, {
