72a10dc99420898bc62f224cc127361bc883c26d
Adson Gomes Oliveira <adsongoliveira2022@outlook.com>
2023-04-04 09:23:34 -0300

    <feat> See More info done

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 // import MainHeader from '../components/header/MainHeader';
 import MainHero from '../components/header/MainHero';
 import HomepageCategoryPreview from '../components/homepage/HomepageCategoryPreview';
+import HomepageInfoSeeMore from '../components/homepage/HomepageInfoSeeMore';
 
 function Homepage() {
   return (
     <>
       <MainHero />
       <HomepageCategoryPreview />
+      <HomepageInfoSeeMore />
     </>
   );
 }
