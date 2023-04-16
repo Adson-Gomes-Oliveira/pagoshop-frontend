8e40eb2134083067a5623d2b597840b3ef7c0303
Adson Gomes Oliveira <adsongoliveira2022@outlook.com>
2023-04-16 09:11:27 -0300

    <feat> Add to cart func done

--- src/components/products/ProductCard.jsx
+++ src/components/products/ProductCard.jsx
@@ -1,13 +1,49 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { v4 as uuid } from 'uuid';
 import PropTypes from 'prop-types';
 // import { Link } from 'react-router-dom';
+import PagoShopContext from '../../context/PagoShopContext';
 import formatNumberToPrice from '../../helpers/formatNumber';
 import './styles/ProductCard.css';
 
 const NO_IMG_URL = 'https://thumbs.dreamstime.com/b/no-thumbnail-image-placeholder-forums-blogs-websites-148010362.jpg';
 
 function ProductCard({ products }) {
+  const { setShowPreviewCartModel } = useContext(PagoShopContext);
+
+  const increaseProductQuantityInCart = (parsedCart, productInCart) => {
+    const productIndex = parsedCart.findIndex((prod) => prod.name === productInCart.name);
+    const newCart = parsedCart;
+    newCart[productIndex].quantity += 1;
+
+    localStorage.setItem('shopping-cart', JSON.stringify(newCart));
+  };
+
+  const addProductToCart = (parsedCart, productToAdd) => {
+    const newCart = parsedCart;
+    const newProduct = {
+      id: productToAdd._id,
+      name: productToAdd.product,
+      thumbnail: productToAdd.thumbnail,
+      price: productToAdd.unitPrice,
+      quantity: 1,
+    };
+
+    newCart.push(newProduct);
+
+    localStorage.setItem('shopping-cart', JSON.stringify(newCart));
+  };
+
+  const handleAddToCartClick = (product) => {
+    const parsedCart = JSON.parse(localStorage.getItem('shopping-cart'));
+    const productInCart = parsedCart.find((prod) => prod.name === product.product);
+
+    if (productInCart) increaseProductQuantityInCart(parsedCart, productInCart);
+    if (!productInCart) addProductToCart(parsedCart, product);
+
+    setShowPreviewCartModel(true);
+  };
+
   const cards = products && products.map((product) => {
     const { product: name, unitPrice, thumbnail } = product;
 
@@ -19,7 +55,12 @@ function ProductCard({ products }) {
         <div className="card-info">
           <span>{name}</span>
           <span>{`R$ ${formatNumberToPrice(unitPrice)}`}</span>
-          <button type="button">Adicionar ao carrinho</button>
+          <button
+            type="button"
+            onClick={() => handleAddToCartClick(product)}
+          >
+            Adicionar ao carrinho
+          </button>
         </div>
       </div>
     );
