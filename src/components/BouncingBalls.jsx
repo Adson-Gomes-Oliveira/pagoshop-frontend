ac73f054b2a4f514844cbc09fde5988dfeb5fc06
Adson Gomes Oliveira <adsongoliveira2022@outlook.com>
2023-04-24 15:44:58 -0300

    <feat> Adding bouncing balls to loading

---
+++ src/components/BouncingBalls.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './styles/BouncingBalls.css';
+
+function BouncingBalls() {
+  return (
+    <>
+      <div className="ball" />
+      <div className="ball" />
+      <div className="ball" />
+    </>
+  );
+}
+
+export default BouncingBalls;
